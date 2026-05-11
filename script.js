d7caeae821bec516e490041b5ecc515a64f024b9
Stefan Taitano <stefantaitano@gmail.com>
2026-05-10 23:47:11 -0500

    Create three variavles and operate function

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-console.log("Hello world");
+function add(firstNum, secondNum) {
+    return firstNum + secondNum
+}
+
+function subtract(firstNum, secondNum) {
+    return firstNum - secondNum
+}
+
+function multiply(firstNum, secondNum) {
+    return firstNum * secondNum
+}
+
+function divide(firstNum, secondNum) {
+    return firstNum / secondNum
+}
+
+let firstNumber
+let operator
+let secondNumber
+
+function operate(operator, numOne, numTwo) {
+    return operator(numOne, numTwo)
+}
